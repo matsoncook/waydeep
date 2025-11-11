0e77529f0501f6c1d47e2573525c2341fadc0984
Mark Cook <mark.cook@airways.co.nz>
2025-11-12 11:43:37 +1300

    cuda

---
+++ cuda.py
@@ -0,0 +1,4 @@
+#Download Installer for Linux Ubuntu 24.04 x86_64
+
+# wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run
+# sudo sh cuda_12.6.0_560.28.03_linux.run
